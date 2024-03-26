acee3f4bfb338976b5804d06a614908545a46372
Sultan Ahmed <146020547+Faizanmedico@users.noreply.github.com>
2024-03-27 03:17:44 +0500

    Add files via upload

---
+++ ex.6.js
@@ -0,0 +1,4 @@
+var names = ["Ahmed", "Sultan", "Rasheed"];
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ ex.6.ts
@@ -0,0 +1,4 @@
+let names: string[] = ["Ahmed", "Sultan", "Rasheed"];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
