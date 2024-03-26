4b28525b068cf101a9454aa9d8001a4a68203f82
Sultan Ahmed <146020547+Faizanmedico@users.noreply.github.com>
2024-03-27 01:57:44 +0500

    Add files via upload

---
+++ ex.2.js
@@ -0,0 +1,4 @@
+var name1 = "Sultan Ahmed"; // This saves the name
+console.log(name1.toLowerCase()); // Shows the name in all small letters
+console.log(name1.toUpperCase()); // Shows the name in ALL BIG LETTERS
+console.log(name1.charAt(0).toUpperCase() + name1.slice(1).toLowerCase()); // Shows the name with the First Letter Big
---
+++ ex.2.ts
@@ -0,0 +1,5 @@
+let name1: string = "Sultan Ahmed"; // This saves the name
+
+console.log(name1.toLowerCase()); // Shows the name in all small letters
+console.log(name1.toUpperCase()); // Shows the name in ALL BIG LETTERS
+console.log(name1.charAt(0).toUpperCase() + name1.slice(1).toLowerCase()); // Shows the name with the First Letter Big
