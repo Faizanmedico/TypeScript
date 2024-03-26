296811f42b27dbba6bdd7dc3d4422111e524013a
Sultan Ahmed <146020547+Faizanmedico@users.noreply.github.com>
2024-03-27 01:15:51 +0500

    Add files via upload

---
+++ ex.1.js
@@ -0,0 +1,2 @@
+var name1 = "Sultan Ahmed"; // This saves the name
+console.log("Hello ".concat(name1, ", would you like to learn some TypeScript today?")); // This shows the messagec
---
+++ ex.1.ts
@@ -0,0 +1,2 @@
+let name1: string = "Sultan Ahmed"; // This saves the name
+console.log(`Hello ${name1}, would you like to learn some TypeScript today?`); // This shows the messagec
