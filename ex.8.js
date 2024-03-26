b8b21009de6933fc8ebc7460e1fb04e7dade0883
Sultan Ahmed <146020547+Faizanmedico@users.noreply.github.com>
2024-03-27 04:01:41 +0500

    Add files via upload

---
+++ ex.8.js
@@ -0,0 +1,4 @@
+var guests = ["Ali", "Arsalan", "Sultan Ahmed"];
+guests.forEach(function (guest) {
+    console.log("Dear ".concat(guest, ", would you like to join me for dinner?"));
+});
---
+++ ex.8.ts
@@ -0,0 +1,5 @@
+let guests: string[] = ["Ali", "Arsalan", "Sultan Ahmed"];
+
+guests.forEach(guest => {
+    console.log(`Dear ${guest}, would you like to join me for dinner?`);
+});
